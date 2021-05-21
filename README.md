2ba136891b12cb01d1d55822c49392469a22d287
Lev Angel <lev@rage-script.ru>
2021-05-21 19:10:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This is a simple Discord bot that can parse RSS feeds and send updates to specified text channels.
+You can add multiple feeds.
+
+###Installation
+`npm install`
+
+Rename `config-example.json` to `config.json` and fill in the correct values.
+
+###Usage
+`node index.js`
+
+###Author
+:shipit: Lev Angel
---
+++ index.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+const Parser  = require('rss-parser');
+const fs      = require('fs').promises;
+
+const client = new Discord.Client();
+const rss    = new Parser();
+const config = require('./config.json');
+
+client.on('ready', () => {
+    console.log('Client ready!');
+    client.user.setActivity(config.botActivity);
+    checkRssFeeds();
+    setInterval(checkRssFeeds, config.checkInterval * 60 * 1000);
+});
+
+client.login(config.token);
+
+
+async function checkRssFeeds(){
+    const lastCheck = await getLastCheckTimestamp();
+
+    for (const channel in config.channels){
+        await parseChannel(channel, lastCheck);
+    }
+
+    await setLastCheckTimestamp();
+}
+
+async function parseChannel(channel, lastCheck){
+    const { feed, channelId, type } = config.channels[channel];
+    const discordChannel    = client.channels.cache.get(channelId);
+    const rssFeed           = await rss.parseURL(feed);
+
+    rssFeed.items.reverse().forEach(item => {
+        const { title, link, isoDate} = item;
+        if(lastCheck > getRecordPubTimestamp( isoDate )) return;
+
+        sendEmbed(discordChannel, title, link, type);
+    });
+}
+
+function sendEmbed(channel, title, description, footer){
+    const embed = new Discord.MessageEmbed()
+        .setTitle(title)
+        .setColor(0x7d7d7d)
+        .setDescription(description)
+        .setFooter(footer);
+    channel.send(embed);
+}
+
+async function getLastCheckTimestamp(){
+    try {
+        return await fs.readFile( __dirname + '/.lastcheck', 'utf8');
+    } catch(e) {
+        return Date.now();
+    }
+}
+
+async function setLastCheckTimestamp(){
+    try {
+        return await fs.writeFile( __dirname + '/.lastcheck', Date.now().toString());
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+// Convert date 2020-05-23T10:00:16.000Z to timestamp in seconds
+function getRecordPubTimestamp(isoDate){
+    return Date.parse( isoDate ) / 1000;
+}
